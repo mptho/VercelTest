3fe988a993a127c461244635c3390ca962037636
mptho <105695403+mptho@users.noreply.github.com>
2024-11-11 14:56:08 +0700

    Delete pages directory

--- pages/api/getDomain.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-  // Lấy domain từ header của yêu cầu và thêm protocol 'https://'
-  const domain = `https://${req.headers.host}`; 
-  res.status(200).json({ domain: domain });
-}
